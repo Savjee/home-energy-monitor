7c3243b221da14be4f58ef25274b01e95183f62f
Xavier Decuyper <hi@savjee.be>
2019-02-28 10:59:23 +0100

    Added unit test for CalculateKwh + run unit test before deploying

---
+++ src-aws/tests/CalculateKwh.test.js
@@ -0,0 +1,28 @@
+const {calculateKWH} = require('../core/helpers');
+const assert = require('assert');
+
+describe('Calculate kWh', function() {
+    it('should return 1 when consuming 1000W for 1 hour', function() {
+
+    	// Consume 1000W for exactly 1 hour
+    	const data = [
+    		[new Date(1*1000), 1000],
+    		[new Date(60*60*1000 +1000), 0],
+    	];
+
+    	// Should be 1kWh (at night)
+      	assert.equal(calculateKWH(data).night, 1);
+	});
+
+	it('should return 0.5 when consuming 1000W for 30min', function() {
+
+    	// Consume 1000W for exactly 30min
+    	const data = [
+    		[new Date(1*1000), 1000],
+    		[new Date(30*60*1000 +1000), 0],
+    	];
+
+    	// Should be 1kWh (at night)
+      	assert.equal(calculateKWH(data).night, 0.5);
+	});
+});
