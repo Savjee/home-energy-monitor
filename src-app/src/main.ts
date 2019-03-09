c70aa283cc579692ad2faa92e7f59b6b8b16b328
Xavier Decuyper <hi@savjee.be>
2019-03-09 14:46:23 +0100

    Remove chart.js from dependencies

--- src-app/src/main.ts
+++ src-app/src/main.ts
@@ -4,17 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { Chart } from 'chart.js';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-
 if (environment.production) {
   enableProdMode();
 }
-
-// Disable the Chart Data Labels plugin by default. We will enable it
-// on a case by case basis.
-Chart.plugins.unregister(ChartDataLabels);
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
