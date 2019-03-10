4ae8c133dd124d7a0dc5b2477826cf2fd583f3ab
Xavier Decuyper <hi@savjee.be>
2019-03-10 16:16:36 +0100

    Refactored home page + refresh data when resumed from homepage

--- src-app/src/app/tab-home/tab-home.page.ts
+++ src-app/src/app/tab-home/tab-home.page.ts
@@ -22,11 +22,39 @@ export class TabHomePage {
   constructor(public energyService: EnergyService) { }
 
   /**
-   * Called when the user reaches the main screen. Here we should determn what
-   * should happen with the background refresh operations (cancel them or
-   * schedule new ones)
+   * Called when the page has been loaded for the very first time. Does not
+   * re-fire when the page is opened a second time.
+   */
+  async ionViewDidLoad() {
+    // When the app resumes from the background we should see if we have to
+    // refresh the data or not. Same logic as when the app first boots.
+    document.addEventListener("resume", async () => {
+      await this.refreshDataIfNeeded();
+    }, false);
+  }
+
+  /**
+   * Called everytime the screen enters the view, regardless of wether it has
+   * been loaded before or not.
    */
   async ionViewWillEnter() {
+    await this.refreshDataIfNeeded();
+  }
+
+  /**
+   * Called when the user leaves the main screen. Here we should cancel the
+   * timer that automatically refreshes the screen.
+   */
+  async ionViewDidLeave() {
+    clearInterval(this.intervalTimer);
+  }
+
+  /**
+   * Determns what should happen with the background refresh operations
+   * (cancel them or schedule new ones). This function should be called
+   * everytime the page is opened or when the app is resumed.
+   */
+  private async refreshDataIfNeeded() {
     // Define how long we should wait in between refreshed (30 seconds)
     const minimumWaitTime = 30 * 1000;
 
@@ -60,14 +88,6 @@ export class TabHomePage {
   }
 
   /**
-   * Called when the user leaves the main screen. Here we should cancel the
-   * timer that automatically refreshes the screen.
-   */
-  async ionViewDidLeave() {
-    clearInterval(this.intervalTimer);
-  }
-
-  /**
    * Schedules a refresh action after a given delay of waitMs milliseconds.
    * Also cancels any pending timeout should there be one.
    * @param waitMs Time to wait in milliseconds
@@ -86,9 +106,11 @@ export class TabHomePage {
   }
 
   /**
+   * Actually fetches data from the backend. Should not be called more then
+   * once every 30 seconds because there are no new datapoints anyway.
    *
    * @param all Wether or not to fetch the "always_on" and "today_so_far"
-   *            Best to set these to false if not needed
+   *            Best to set these to false if not needed (better performance)
    */
   private async fetchData(all?: boolean) {
     const realtime = await this.energyService.getHomePageStatistics(all);
